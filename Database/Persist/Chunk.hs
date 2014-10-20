0f238064305880380a4ecf52a0df77fdd2d80a50
Rehno Lindeque <errantkid@gmail.com>
2014-10-20 17:05:49 +0200

    Add two chunked select queries (for keys and entities)

--- Database/Persist/Chunk.hs
+++ Database/Persist/Chunk.hs
@@ -1,1 +1,43 @@
+{-# LANGUAGE TypeFamilies #-}
 module Database.Persist.Chunk where
+
+import Database.Persist
+import qualified Data.Conduit                 as C
+import qualified Data.Conduit.Combinators     as C
+import qualified Data.Conduit.List            as CL
+import Control.Monad (unless)
+
+-- | Entities are fetched in chunks using 'OffsetBy' and 'LimitTo'
+-- TODO: Could this be used lazily? e.g.
+-- >>> take 10 $ selectSourceChunked 5 True [] :: Entity Foo
+selectSourceChunked :: ( PersistEntity val
+                       , PersistEntityBackend val ~ PersistMonadBackend m
+                       , PersistQuery m
+                       )
+                    => Int                -- ^ Chunk size
+                    -> [Filter val]
+                    -> C.Source m (Entity val)
+selectSourceChunked chunk filters = loop 0
+  where
+    loop off = do
+      l <- selectSource filters [OffsetBy off, LimitTo chunk] C.$= C.length
+      unless (l < chunk) $ loop (off + chunk)
+
+-- | Keys are fetched in chunks using 'OffsetBy' and 'LimitTo'
+selectKeysChunked :: ( PersistEntity val
+                     , PersistEntityBackend val ~ PersistMonadBackend m
+                     , PersistQuery m
+                     )
+                  => Int                -- ^ Chunk size
+                  -> [Filter val]
+                  -> C.Source m (Key val)
+selectKeysChunked chunk filters = loop 0
+  where
+    loop off = do
+      l <- selectKeys filters [OffsetBy off, LimitTo chunk, Asc persistIdField] C.$= lengthAccumC
+      unless (l < chunk) $ loop (off + chunk)
+
+    -- | Similar to 'Data.Conduit.Combinators.length', but implemented as an
+    --   accumulator instead of a fold
+    lengthAccumC :: (Monad m, Num len) => C.ConduitM i i m len
+    lengthAccumC = CL.mapAccum (\i l -> (l + 1, i)) 0
