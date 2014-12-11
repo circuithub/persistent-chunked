e3128f40b5b6a2738165ed7eeb4f3127559ffcd0
Rehno Lindeque <errantkid@gmail.com>
2014-12-11 16:05:43 +0200

    Start working on reversed versions of queries (result returned in reverse order)

--- Database/Persist/Chunk.hs
+++ Database/Persist/Chunk.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE TypeFamilies #-}
 module Database.Persist.Chunk
   ( selectSourceChunked
+  , selectSourceChunkedReverse
   , selectKeysChunked
+  , selectKeysChunkedReverse
   ) where
 
 import Database.Persist
@@ -31,6 +33,23 @@ selectSourceChunked chunk filters = loop 0
       l <- selectSource filters [OffsetBy off, LimitTo chunk] C.$= lengthAccumC
       unless (l < chunk) $ loop (off + chunk)
 
+-- | Entities are fetched in chunks using 'OffsetBy' and 'LimitTo', in reverse
+selectSourceChunkedReverse :: ( PersistEntity val
+                              , PersistEntityBackend val ~ PersistMonadBackend m
+                              , PersistQuery m
+                              )
+                           => Int                -- ^ Chunk size
+                           -> [Filter val]
+                           -> C.Source m (Entity val)
+selectSourceChunked chunk filters = do
+  total <- P.count filters
+  loop 0
+  where
+    loop off = do
+      let remaining = total - off
+      l <- selectSource filters [OffsetBy (max 0 $ remaining - chunk), LimitTo $ min chunk remaining] C.$= lengthAccumC
+      unless (l < chunk) $ loop (off + chunk)
+
 -- | Keys are fetched in chunks using 'OffsetBy' and 'LimitTo'
 selectKeysChunked :: ( PersistEntity val
                      , PersistEntityBackend val ~ PersistMonadBackend m
@@ -44,3 +63,18 @@ selectKeysChunked chunk filters = loop 0
     loop off = do
       l <- selectKeys filters [OffsetBy off, LimitTo chunk, Asc persistIdField] C.$= lengthAccumC
       unless (l < chunk) $ loop (off + chunk)
+
+-- | Keys are fetched in chunks using 'OffsetBy' and 'LimitTo'
+selectKeysChunkedReverse :: ( PersistEntity val
+                            , PersistEntityBackend val ~ PersistMonadBackend m
+                            , PersistQuery m
+                            )
+                         => Int                -- ^ Chunk size
+                         -> [Filter val]
+                         -> C.Source m (Key val)
+selectKeysChunked chunk filters = loop 0
+  where
+    loop off = do
+      l <- selectKeys filters [OffsetBy off, LimitTo chunk, Asc persistIdField] C.$= lengthAccumC
+      unless (l < chunk) $ loop (off + chunk)
+
