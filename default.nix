386619d2bd72b074240a335ab2a46bdb5cd7b534
Oliver Charles <ollie@ocharles.org.uk>
2015-10-14 11:29:11 +0100

    nix: Add default.nix
    Automatically generated by cabal2nix

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ mkDerivation, base, conduit, conduit-combinators, containers
+, monad-control, persistent, split, stdenv
+}:
+mkDerivation {
+  pname = "persistent-chunked";
+  version = "0.0.3";
+  src = ./.;
+  buildDepends = [
+    base conduit conduit-combinators containers monad-control
+    persistent split
+  ];
+  homepage = "https://github.com/circuithub/persistent-chunked";
+  description = "Helpers for running chunked queries with persistent";
+  license = stdenv.lib.licenses.mit;
+}
